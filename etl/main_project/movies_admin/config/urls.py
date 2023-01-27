af9edb6bdff96254c0999b3e5764baebfa008645
Dmitry R <dmr.main@outlook.com>
2023-01-27 13:05:37 +0300

    changed paths for django api to api_alt

--- etl/main_project/movies_admin/config/urls.py
+++ etl/main_project/movies_admin/config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('movies.api.urls')),
+    path('api_alt/', include('movies.api.urls')),
 ]
