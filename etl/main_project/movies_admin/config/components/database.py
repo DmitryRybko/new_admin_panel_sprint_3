fa7fe58f1282c157e467a4d5d743af59cd9742de
Alex Kruglow <alexkruglow@gmail.com>
2023-05-16 02:10:41 +0300

    Fixes for the final sprint.

--- etl/main_project/movies_admin/config/components/database.py
+++ etl/main_project/movies_admin/config/components/database.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST_INTERNAL'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', 54321),
         'OPTIONS': {
             # Нужно явно указать схемы, с которыми будет работать приложение.
--- etl/main_project/movies_admin/entrypoint.sh
+++ etl/main_project/movies_admin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-/opt/app/wait-for-it.sh "${DB_HOST}":"${DB_PORT}"  -- echo "Postgres is up"
+bash /opt/app/wait-for-it.sh "${DB_HOST}":"${DB_PORT}"  -- echo "Postgres is up"
 python manage.py collectstatic --noinput
 python manage.py compilemessages -l en -l ru
 python manage.py migrate
